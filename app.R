ab26c1569ddad6ecaed10fd3db084e7bc99c6d90
Andrew Baxter <andrewbaxter439@gmail.com>
2021-03-12 10:15:05 +0000

    working with named args to clicks object and printing vals

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(shiny)
+library(glue)
 
 ui <- fluidPage(
     titlePanel("Old Faithful Geyser Data"),
@@ -19,7 +20,7 @@ ui <- fluidPage(
     mainPanel(
         plotOutput("distPlot", click = "click_graph"),
         
-        textOutput("coords")
+        verbatimTextOutput("coords")
     )))
 
 # Define server logic required to draw a histogram
@@ -28,15 +29,15 @@ server <- function(input, output) {
     clicks <- reactiveValues(clickno = 1)
     
     observeEvent(input$click_graph$x, {
-        clicks[[paste0("c", clicks$clickno)]] <- c(input$click_graph$x, input$click_graph$y)
+        clicks[[paste0("c", clicks$clickno)]] <- list(x = input$click_graph$x, y = input$click_graph$y)
         clicks$clickno <- 3 - clicks$clickno
     })
     
     lineDraw <- reactiveValues()
     
-    observeEvent(clicks$c2[1],{
-        grad <- (clicks$c1[2]-clicks$c2[2])/(clicks$c1[1] - clicks$c2[1])
-        int <- clicks$c1[2]- clicks$c1[1] * grad
+    observeEvent(clicks$c2$x,{
+        grad <- (clicks$c1$y-clicks$c2$y)/(clicks$c1$x - clicks$c2$x)
+        int <- clicks$c1$y- clicks$c1$x * grad
         lineDraw$grad <- grad
         lineDraw$int <- int
     }
@@ -44,7 +45,7 @@ server <- function(input, output) {
     
     output$distPlot <- renderPlot({
         
-        df <- data.frame(x = as.numeric(c(clicks$c1[1], clicks$c2[1])), y = as.numeric(c(clicks$c1[2], clicks$c2[2])))
+        df <- data.frame(x = as.numeric(c(clicks$c1$x, clicks$c2$x)), y = as.numeric(c(clicks$c1$y, clicks$c2$y)))
         
         # draw the histogram with the specified number of bins
         ggplot(faithful, aes(x = eruptions, y = waiting)) +
@@ -58,8 +59,14 @@ server <- function(input, output) {
     })
     
     output$coords <- renderText({
-        paste(clicks$c1, clicks$c2, lineDraw$grad, lineDraw$int)
-        # glue::glue("{clicks$c1}, clicks$c2, lineDraw$grad, lineDraw$int")
+        # paste(clicks$c1, clicks$c2, lineDraw$grad, lineDraw$int)
+        glue("x1 = {clicks$c1$x}, y1 = {clicks$c1$y}
+             x2 = {clicks$c2$x}, y2 = {clicks$c2$y}
+             Slope = {lineDraw$grad}, Intercept = {lineDraw$int}", 
+                   .transformer = function(text, envir) {
+                       # return(round(as.numeric(text), 2))
+                       round(as.numeric(identity_transformer(text, envir)), 2)
+                   })
     })
 }
 
